5d487311826faf7e65657681d6f48e49e11991ba
Sandhykaarya <sandhykaarya@student.telkomuniversity.ac.id>
2022-04-23 17:18:00 +0700

    menambahkan API Movies

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("Movie"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -16,28 +19,87 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/api/Movies", async (TodoDb db) =>
+    await db.Todos.ToListAsync());
 
-app.MapGet("/weatherforecast", () =>
+
+app.MapPost("/api/Movies", async (Movie todo, TodoDb db) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/api/", todo);
+});
+
+/*
+app.MapGet("/api/Movies/{id}", async (int id, Movie inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Title = inputTodo.Title;
+    todo.Director = inputTodo.Director;
+    todo.Stars = inputTodo.Stars;
+    todo.Desription = inputTodo.Desription;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+}); */
+
+app.MapGet("/api/Movies/{id}", () => "");
+
+app.MapDelete("/api/Movies/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Movie todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.Ok(todo);
+    }
+
+    return Results.NotFound();
+});
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+class Movie
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public string Title { get; set; }
+    public string Director { get; set; }
+    public List<string> Stars { get; set; }
+    public string Desription { get; set; }
+
+    public Movie(string Title, string Director, List<string> stars, string Description)
+    {
+        this.Title = Title;
+        this.Director = Director;
+        this.Stars = stars;
+        this.Desription = Description;
+    }
 }
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Movie> Todos => Set<Movie>();
+}
+
+/*app.MapGet("/api/Movies/{id}", async (int id, Movie inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Title = inputTodo.Title;
+    todo.Director = inputTodo.Director;
+    todo.Stars = inputTodo.Stars;
+    todo.Desription = inputTodo.Desription;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});*/
